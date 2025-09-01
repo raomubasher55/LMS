825c1a21d2d3ec60b3518b89beacfafe219c5f34
zahidghotia786 <zahidghotia@gmail.com>
2025-09-01 11:02:21 -0700

    Added  updates by Zahid

--- src/app/blogs-dark/[id]/page.js
+++ src/app/blogs-dark/[id]/page.js
@@ -1,28 +1,7 @@
-import BlogDetailsMain from "@/components/layout/main/BlogDetailsMain";
-import ThemeController from "@/components/shared/others/ThemeController";
-import PageWrapper from "@/components/shared/wrappers/PageWrapper";
-import blogs from "@/../public/fakedata/blogs.json";
-import { notFound } from "next/navigation";
-export const metadata = {
-  title: "Blog Details - Dark | Edurock - Education LMS Template",
-  description: "Blog Details - Dark | Edurock - Education LMS Template",
-};
-const Blog_Details_Dark = ({ params }) => {
-  const { id } = params;
-  const isExistBlog = blogs?.find(({ id: id1 }) => id1 === parseInt(id));
-  if (!isExistBlog) {
-    notFound();
-  }
+import React from 'react'
+
+export default function page() {
   return (
-    <PageWrapper>
-      <main className="is-dark">
-        <BlogDetailsMain />
-        <ThemeController />
-      </main>
-    </PageWrapper>
-  );
-};
-export async function generateStaticParams() {
-  return blogs?.map(({ id }) => ({ id: id.toString() }));
+    <div>page</div>
+  )
 }
-export default Blog_Details_Dark;
